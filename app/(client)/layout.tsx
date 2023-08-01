87327cc5baef5cd99b9938b4dd97cd4fa8878911
jkhaui <jordan.lee.163@gmail.com>
2023-08-01 17:33:10 +1000

    fix: attempt `vanilla-extract` css chunking fix in prod

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -5,9 +5,7 @@ import { KonstaProvider } from 'konsta/react';
 import { TITLE } from '@/app/constants';
 import { AnimatePresence } from 'framer-motion';
 import { TabNavBar } from '@/components';
-// import { useIonicLoader } from '@/hooks';
-import { initialize } from '@ionic/core/components';
-import { defineCustomElements } from '@ionic/core/loader';
+import { useIonicLoader } from '@/hooks';
 
 import '../globals.css';
 
@@ -30,19 +28,7 @@ import 'overlayscrollbars/overlayscrollbars.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function ClientLayout({ tabs }) {
-  // useIonicLoader();
-
-  React.useEffect(() => {
-    // `useEffect` runs only on the client.
-    // Therefore, this is the phase during which client-side hydration occurs.
-    initialize({
-      // Forcing the global Ionic mode `ios` to confirm that the setup is working
-      // with a custom configuration.
-      mode: 'ios',
-    });
-
-    defineCustomElements(window);
-  }, []);
+  useIonicLoader();
 
   return (
     <KonstaProvider theme={'parent'}>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // https://github.com/vanilla-extract-css/vanilla-extract/issues/1085#issuecomment-1555986222
+    config.optimization.splitChunks = false;
+
+    return config;
+  },
   images: {
     // Next's image optimization breaks capacitor
     unoptimized: true,
