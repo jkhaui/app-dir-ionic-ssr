6048626694e2adde4ada0fd9ac4a935e069fe89e
jkhaui <jordan.lee.163@gmail.com>
2023-08-01 17:19:53 +1000

    fix: try reverting custom ion loader hook

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -5,7 +5,9 @@ import { KonstaProvider } from 'konsta/react';
 import { TITLE } from '@/app/constants';
 import { AnimatePresence } from 'framer-motion';
 import { TabNavBar } from '@/components';
-import { useIonicLoader } from '@/hooks';
+// import { useIonicLoader } from '@/hooks';
+import { initialize } from '@ionic/core/components';
+import { defineCustomElements } from '@ionic/core/loader';
 
 import '../globals.css';
 
@@ -28,7 +30,19 @@ import 'overlayscrollbars/overlayscrollbars.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function ClientLayout({ tabs }) {
-  useIonicLoader();
+  // useIonicLoader();
+
+  React.useEffect(() => {
+    // `useEffect` runs only on the client.
+    // Therefore, this is the phase during which client-side hydration occurs.
+    initialize({
+      // Forcing the global Ionic mode `ios` to confirm that the setup is working
+      // with a custom configuration.
+      mode: 'ios',
+    });
+
+    defineCustomElements(window);
+  }, []);
 
   return (
     <KonstaProvider theme={'parent'}>
