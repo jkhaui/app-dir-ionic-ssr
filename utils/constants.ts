a56b3ec3ff61c4fc785d0ce4af6ee3339710c38b
jkhaui <jordan.lee.163@gmail.com>
2023-08-09 13:55:02 +1000

    refactor: modularise breakpoints object

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,10 @@
 export const TITLE = 'Next App Dir + Ionic + Konsta: SSR';
 export const ROOT_PATH = '/';
+
+export const breakpoints = {
+  xs: '0',
+  sm: { max: '576px' },
+  md: { max: '768px' },
+  lg: '1136px',
+  xl: '1200px',
+};
